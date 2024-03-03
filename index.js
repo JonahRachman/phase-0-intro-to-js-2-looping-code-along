918968848cdd64240403775f5af5c63b95b518e9
Jonah Rachman <jonahrachman76@gmail.com>
2024-03-03 17:53:43 -0500

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(arrayName, giftType) {
+    const arrayCard = []
+    for (let i = 0; i < arrayName.length; i++) {
+        arrayCard.push(`Thank you, ${arrayName[i]}, for the wonderful ${giftType} gift!`);
+    }
+    return arrayCard
+}
+
+function countDown(n) {
+    for (let i = n; i >= 0; i--) {
+        console.log(i);
+    }
+}
